64180723ad31e3fa0e11027b9f9d6c6dbfcd8bea
rafael <rafaelmarinhoalves5@gmail.com>
2023-11-13 01:03:03 -0300

    melhorias no Main

--- src/br/com/votehub/Main.java
+++ src/br/com/votehub/Main.java
@@ -2,10 +2,13 @@ package br.com.votehub;
 
 import java.util.Scanner;
 
+import br.com.votehub.model.DAOs.VotoDAO;
+import br.com.votehub.model.DAOs.candidatoDAO;
 import br.com.votehub.model.DAOs.votanteDAO;
 import br.com.votehub.model.criptografia.CriptografiaVotante;
-import br.com.votehub.model.criptografia.Encriptador;
+import br.com.votehub.model.vo.Candidato;
 import br.com.votehub.model.vo.Votante;
+import br.com.votehub.model.vo.Voto;
 
 public class Main {
 
@@ -13,8 +16,30 @@ public class Main {
 		System.out.println(CriptografiaVotante.obterChaveSecreta());
 
 		Main programa = new Main();
-		programa.addVotante();
+		programa.operação();
 	}
+
+	public void operação() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("operação 1: adicionar votante");
+		System.out.println("operação 2: adicionar candidato");
+		System.out.println("operação 3: adicionar voto");
+		int op = sc.nextInt();
+				if(op == 1) {
+					addVotante();
+				}
+				if(op == 2) {
+					addCandidato();
+				}
+				if(op == 3) {
+					addVoto();
+				}else {
+					System.out.println("digite uma operação valida");
+				}
+		
+	}
+
+	
 	
 	private void addVotante() {
 		Scanner sc = new Scanner(System.in);
@@ -33,8 +58,9 @@ public class Main {
 		vdao.addVotante(v);
 		sc.close();
 	}
-	
-	/*private void addCandidato() {
+
+	private void addCandidato() {
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Nome do candidato:");
 		String nome = sc.next();
@@ -42,14 +68,15 @@ public class Main {
 		int id = sc.nextInt();
 		System.out.println("Cargo do candidato:");
 		String cargo = sc.next();
-		
+
 		Candidato c = new Candidato(nome, id, cargo);
 		candidatoDAO cdao = new candidatoDAO();
 		cdao.addCandidato(c);
 		sc.close();
+
 	}
 
-	 private void addVoto() {
+	private void addVoto() {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Digite o id do voto: ");
 		int id = sc.nextInt();
@@ -61,10 +88,8 @@ public class Main {
 		Voto vt = new Voto(id, id_candidato, id_votante);
 
 		VotoDAO vtdao = new VotoDAO();
-		vtdao.addvoto(vt);
+		vtdao.addVoto(vt);
 		sc.close();
-	}*/
+	}
 
-
-	
 }
