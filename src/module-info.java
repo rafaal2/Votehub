ebda8329b59ce015246be10b21dbb6f960b5399b
eron <eron13.1reis@gmail.com>
2023-11-17 18:47:00 -0300

    Conversao para maven. Adicinado dependencias jasypt e mysql connector

--- src/module-info.java
+++ src/module-info.java
@@ -4,15 +4,15 @@
 /**
  * 
  */
-module Criptografia {
-	exports br.com.votehub.controller;
-	exports br.com.votehub.model.criptografia;
-	exports br.com.votehub.model.DAOs;
-	exports br.com.votehub;
-	exports br.com.votehub.model.vo;
-	exports br.com.votehub.view;
-
-	requires java.desktop;
-	requires java.sql;
-	requires jasypt;
-}
+//module Criptografia {
+//	exports br.com.votehub.controller;
+//	exports br.com.votehub.model.criptografia;
+//	exports br.com.votehub.model.DAOs;
+//	exports br.com.votehub;
+//	exports br.com.votehub.model.vo;
+//	exports br.com.votehub.view;
+//
+//	requires java.desktop;
+//	requires java.sql;
+//	requires jasypt;
+//}
