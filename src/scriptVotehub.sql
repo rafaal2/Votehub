72bbd94d6d310fd950210fa6b303f8180b62642c
rafaal2 <113396635+rafaal2@users.noreply.github.com>
2023-11-17 21:36:21 -0300

    adicionado o script do banco de dados final
    versão final do banco de dados da aplicação com novas tabelas como votação e adm, e a atualização das tabelas antigas.

--- src/scriptVotehub.sql
+++ src/scriptVotehub.sql
@@ -1,116 +1,67 @@
--- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
---
--- Host: localhost    Database: coursejdbc
--- ------------------------------------------------------
--- Server version	8.0.35
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 create database votehub;
 use votehub;
---
--- Table structure for table `candidato`
---
-
-DROP TABLE IF EXISTS `candidato`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `candidato` (
-  `id` int NOT NULL,
-  `nome` varchar(200) DEFAULT NULL,
-  `id_cargo` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `candidato`
+-- criando estrutura da tabela `adm`
 --
 
-LOCK TABLES `candidato` WRITE;
-/*!40000 ALTER TABLE `candidato` DISABLE KEYS */;
-INSERT INTO `candidato` VALUES (1,'cartaxo','reitor'),(2,'flavio','reitor'),(3,'mercio','reitor');
-/*!40000 ALTER TABLE `candidato` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE adm (
+  id_adm int NOT NULL AUTO_INCREMENT,
+  login varchar(200) DEFAULT NULL,
+  senha varchar(100) DEFAULT NULL,
+  PRIMARY KEY (id_adm)
+) ;
 
 --
--- Table structure for table `votante`
+-- criando estrutura da tabela `candidato`
 --
 
-DROP TABLE IF EXISTS `votante`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `votante` (
-  `id` int NOT NULL,
-  `nome` varchar(100) DEFAULT NULL,
-  `cpf` char(12) DEFAULT NULL,
-  `ocupação` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE candidato (
+  numero_candidato varchar(100) NOT NULL,
+  nome char(200) DEFAULT NULL,
+  cargo char(100) DEFAULT NULL,
+  PRIMARY KEY (numero_candidato)
+) ;
 
 --
--- Dumping data for table `votante`
+-- criando estrutura da tabela `votante`
 --
 
-LOCK TABLES `votante` WRITE;
-/*!40000 ALTER TABLE `votante` DISABLE KEYS */;
-INSERT INTO `votante` VALUES (1,'rafael','12121212','aluno'),(2,'helder','15111111','aluno'),(3,'brayan','13113131','aluno'),(4,'kaio','14113131','aluno'),(5,'eron','15113131','aluno'),(6,'jean','16113131','aluno'),(7,'artur wayans','17113131','aluno'),(8,'adonai','18113131','aluno');
-/*!40000 ALTER TABLE `votante` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE votante (
+  id_votante int NOT NULL AUTO_INCREMENT,
+  matricula char(200) DEFAULT NULL,
+  nome varchar(200) DEFAULT NULL,
+  senha varchar(100) DEFAULT NULL,
+  ocupação char(1) DEFAULT NULL,
+  PRIMARY KEY (id_votante)
+);
 
 --
--- Table structure for table `voto`
+-- criando estrutura da tabela `votação`
 --
 
-DROP TABLE IF EXISTS `voto`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `voto` (
-  `id` int NOT NULL,
-  `id_candidato` int DEFAULT NULL,
-  `id_votante` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_candidato` (`id_candidato`),
-  KEY `id_votante` (`id_votante`),
-  CONSTRAINT `voto_ibfk_1` FOREIGN KEY (`id_candidato`) REFERENCES `candidato` (`id`),
-  CONSTRAINT `voto_ibfk_2` FOREIGN KEY (`id_votante`) REFERENCES `votante` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE votação (
+  id_votação int NOT NULL AUTO_INCREMENT,
+  data_inicio datetime DEFAULT NULL,
+  data_fim datetime DEFAULT NULL,
+  PRIMARY KEY (id_votação)
+);
 
 --
--- Dumping data for table `voto`
+-- criando estrutura da tabela `voto`
 --
 
-LOCK TABLES `voto` WRITE;
-/*!40000 ALTER TABLE `voto` DISABLE KEYS */;
-INSERT INTO `voto` VALUES (1,2,1),(2,1,2),(3,1,3),(4,1,4),(5,2,5);
-/*!40000 ALTER TABLE `voto` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE voto (
+  id_voto int NOT NULL AUTO_INCREMENT,
+  numero_candidato varchar(100) DEFAULT NULL,
+  id_votante int DEFAULT NULL,
+  PRIMARY KEY (id_voto),
+  KEY numero_candidato (numero_candidato),
+  KEY id_votante (id_votante),
+  CONSTRAINT voto_ibfk_1 FOREIGN KEY (numero_candidato) REFERENCES candidato (numero_candidato),
+  CONSTRAINT voto_ibfk_2 FOREIGN KEY (id_votante) REFERENCES votante (id_votante)
+)
 
---
--- Dumping events for database 'coursejdbc'
---
 
---
--- Dumping routines for database 'coursejdbc'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-11-07 23:50:06
