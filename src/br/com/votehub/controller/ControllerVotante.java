de3f9e8023482ac3cbb53bac894ddc6cefd8a18f
Kaio Vinícius <kaiovsbarbosa@gmail.com>
2023-11-21 15:12:23 -0300

    Escopo da classe ControllerVotante

--- src/br/com/votehub/controller/ControllerVotante.java
+++ src/br/com/votehub/controller/ControllerVotante.java
@@ -1,5 +1,24 @@
 package br.com.votehub.controller;
 
+import br.com.votehub.model.vo.Votante;
+import br.com.votehub.model.DAOs.*;
+
 public class ControllerVotante {
 
+	private VotanteDAO votanteRepository = new VotanteDAO();
+
+//	public void registrarVotante(String matricula, String nome, String senha, String ocupacao) throws BusinessException {
+//	    this.validarRegistro();
+//	    
+//	    Votante vt = new Votante(0, matricula, nome, senha, ocupacao);
+//	    votanteRepository.addVotante(vt);
+//	}
+
+	public void validarRegistro(String matricula, String nome, String senha, String ocupacao) throws BusinessException {
+	    if(matricula.isBlank() || nome.isBlank() || senha.isBlank() || ocupacao.isBlank()) {            
+	        throw new BusinessException("Todos os campos devem estar preenchidos!");            
+	    }
+	}
+
 }
+
