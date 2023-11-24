b8afd44e3a140702a105a95e3c8bf2dd04abee4f
rafael <rafaelmarinhoalves5@gmail.com>
2023-11-23 21:23:18 -0300

    exceções do login e senha adm

--- src/br/com/votehub/controller/ControllerAdm.java
+++ src/br/com/votehub/controller/ControllerAdm.java
@@ -12,51 +12,52 @@ import br.com.votehub.model.vo.Adm;
 
 public class ControllerAdm {
 	private AdmDAO AdmRepository = new AdmDAO();
-	
-	
-	public void registrarAdm(int id_adm, String login, String senha)
-			throws BusinessException {
+
+	public void registrarAdm(int id_adm, String login, String senha) throws BusinessException {
 		this.validarRegistro(id_adm, login, senha);
 
 		Adm a = new Adm(id_adm, login, senha);
 		AdmRepository.addAdm(a);
 	}
+
 	public void validarRegistro(int id_adm, String login, String senha) throws BusinessException {
-		if(login.isBlank() || senha.isBlank()) {			
-			throw new BusinessException("Todos os campos devem estar preenchindos!");			
+		if (login.isBlank() || senha.isBlank()) {
+			throw new BusinessException("Todos os campos devem estar preenchindos!");
 		}
-		
-		if(login.length() > 100 || senha.length() > 100) {
+
+		if (login.length() > 100 || senha.length() > 100) {
 			throw new BusinessException("Os valores login e senha informados devem possuir limite de 100 caracteres");
 		}
-		
-		if(login.length() > 200) {
+
+		if (login.length() > 200) {
 			throw new BusinessException("O login deve possuir limite de 200 caracteres!");
 		}
 	}
+
 	public void deletarAdm(int id_adm) throws BusinessException {
 		AdmRepository.deleteAdm(id_adm);
 	}
-	//public void validarExclusao(int id_adm) throws BusinessException {
-		//if (AdmRepository.searchVotanteById(id_adm) == null) {
-			//throw new BusinessException("Adm referido não encontrado!");}
-	public void atualizarAdm(int id_adm, String login, String senha)
-			throws BusinessException {
+
+	// public void validarExclusao(int id_adm) throws BusinessException {
+	// if (AdmRepository.searchVotanteById(id_adm) == null) {
+	// throw new BusinessException("Adm referido não encontrado!");}
+	public void atualizarAdm(int id_adm, String login, String senha) throws BusinessException {
 		validarAtualizacao(id_adm, login, senha);
 
 		AdmRepository.updateAdm(id_adm, login, senha);
 	}
-	public void validarAtualizacao(int id_adm, String login, String senha)
-			throws BusinessException {
-		//if (AdmRepository.searchVotanteById(id_votante) == null) {
-			//throw new BusinessException("Votante referido não encontrado");
-		//}
+
+	public void validarAtualizacao(int id_adm, String login, String senha) throws BusinessException {
+		// if (AdmRepository.searchVotanteById(id_votante) == null) {
+		// throw new BusinessException("Votante referido não encontrado");
+		// }
 
 		if (id_adm != 0 || login.isBlank() || senha.isBlank()) {
 			throw new BusinessException("Todos os campos devem estar preenchidos!");
 		}
 
 	}
+
 	static Encriptador encrip = new Encriptador();
 
 	public boolean verificarloginadm(String logindigit) throws BusinessException, SQLException {
@@ -70,11 +71,14 @@ public class ControllerAdm {
 			while (rs.next()) {
 				String matriculabd = encrip.encriptadorDeValores(rs.getString("login"), "d");
 				boolean check = logindigit.equals(matriculabd);
-				if (check == true) {
-					return check;
-				} else {
-					System.out.println("login incorreta");
+				if (check) {
+					return true;
 				}
+				if (logindigit.isBlank()) {
+					throw new BusinessException("o campo de login deve estar preenchido");
+				}
+				throw new BusinessException("login incorreta");
+
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -84,13 +88,6 @@ public class ControllerAdm {
 
 		}
 		return false;
-	}
-
-	public void validarVerificarloginadm(String logindigit) throws BusinessException {
-		if (logindigit.isBlank()) {
-			throw new BusinessException("Todos os campos devem estar preenchidos!");
-		}
-
 	}
 
 	public boolean verificarsenhaadm(String senhadigit) throws BusinessException, SQLException {
@@ -104,13 +101,16 @@ public class ControllerAdm {
 			while (rs.next()) {
 				String senhabd = encrip.encriptadorDeValores(rs.getString("senha"), "d");
 				boolean check = senhadigit.equals(senhabd);
-				if (check == true) {
-					return check;
-				} else {
-					System.out.println("senha incorreta");
-				}
-			}
-		} catch (SQLException e) {
+				if (check) {
+					return true;
+				}if(senhadigit.isBlank()) {
+					throw new BusinessException("o campo da senha deve estar preenchido");
+				}if(senhadigit.length() < 10) {
+					throw new BusinessException("a senha deve conter no minimo 11 caracteres");
+				}}
+			throw new BusinessException("Senha incorreta");
+		}
+		 catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
 			DB.closeResultSet(rs);
@@ -119,14 +119,15 @@ public class ControllerAdm {
 		}
 		return false;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	public void validarVerificarloginadm(String logindigit, String senhadigit) throws BusinessException {
+		if (logindigit.isBlank() || senhadigit.isBlank()) {
+			throw new BusinessException("Todos os campos devem estar preenchidos!");
+		}
+		if (logindigit.length() > 100 || senhadigit.length() > 100) {
+			throw new BusinessException("Os valores numero e cargo informados devem possuir limite de 100 caracteres");
+		}
+
+	}
 
 }
--- src/br/com/votehub/view/ADMLogin.java
+++ src/br/com/votehub/view/ADMLogin.java
@@ -56,50 +56,48 @@ public class ADMLogin extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(new MigLayout("fill", "[grow][][grow][][grow]", "[][][][][][][][]"));
-				
-				JPanel panel = new JPanel();
-				contentPane.add(panel, "cell 2 0,alignx center,growy");
-				panel.setPreferredSize(new Dimension(800, 600)); 
-						panel.setLayout(new MigLayout("fill", "[grow][][grow][][][][grow]", "[][][][][][][][][]"));
-						
-								
-								JLabel lblVoteHub = new JLabel("VoteHub");
-								lblVoteHub.setFont(new Font("Tahoma", Font.BOLD, 17));
-								panel.add(lblVoteHub, "cell 0 0 7 1,alignx center,aligny center");
-						
-						JLabel lblUserLogin = new JLabel("Usuário");
-						lblUserLogin.setFont(new Font("Tahoma", Font.BOLD, 13));
-						panel.add(lblUserLogin, "cell 0 2 2 1,alignx trailing,aligny center");
-						
-						fieldUserLogin = new JTextField();
-						panel.add(fieldUserLogin, "cell 2 2 4 1,growx");
-						fieldUserLogin.setColumns(10);
-						
-						JLabel lblSenhaLogin = new JLabel("Senha");
-						lblSenhaLogin.setFont(new Font("Tahoma", Font.BOLD, 13));
-						panel.add(lblSenhaLogin, "cell 0 4 2 1,alignx trailing,aligny center");
-						
-						fieldSenhaLogin = new JTextField();
-						panel.add(fieldSenhaLogin, "cell 2 4 4 1,growx");
-						
-						JButton btnEntrarLogin = new JButton("Entrar");
-						panel.add(btnEntrarLogin, "cell 0 6 7 1,alignx center,aligny center");
-						btnEntrarLogin.addActionListener(new ActionListener() {
-							public void actionPerformed(ActionEvent e) {
-								String logindigit = fieldUserLogin.getText();
-								String senhadigit = fieldSenhaLogin.getText();
-								try {
-									ControllerAdm contAdm = new ControllerAdm();
-									contAdm.verificarloginadm(logindigit);
-									contAdm.verificarsenhaadm(senhadigit);
-								} catch (SQLException e1) {
-									e1.printStackTrace();
-								} catch (BusinessException error) {
-									JOptionPane.showMessageDialog(null, error.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
-								}
 
-							
-							}
-							});
-							}
-						}
+		JPanel panel = new JPanel();
+		contentPane.add(panel, "cell 2 0,alignx center,growy");
+		panel.setPreferredSize(new Dimension(800, 600));
+		panel.setLayout(new MigLayout("fill", "[grow][][grow][][][][grow]", "[][][][][][][][][]"));
+
+		JLabel lblVoteHub = new JLabel("VoteHub");
+		lblVoteHub.setFont(new Font("Tahoma", Font.BOLD, 17));
+		panel.add(lblVoteHub, "cell 0 0 7 1,alignx center,aligny center");
+
+		JLabel lblUserLogin = new JLabel("Usuário");
+		lblUserLogin.setFont(new Font("Tahoma", Font.BOLD, 13));
+		panel.add(lblUserLogin, "cell 0 2 2 1,alignx trailing,aligny center");
+
+		fieldUserLogin = new JTextField();
+		panel.add(fieldUserLogin, "cell 2 2 4 1,growx");
+		fieldUserLogin.setColumns(10);
+
+		JLabel lblSenhaLogin = new JLabel("Senha");
+		lblSenhaLogin.setFont(new Font("Tahoma", Font.BOLD, 13));
+		panel.add(lblSenhaLogin, "cell 0 4 2 1,alignx trailing,aligny center");
+
+		fieldSenhaLogin = new JTextField();
+		panel.add(fieldSenhaLogin, "cell 2 4 4 1,growx");
+
+		JButton btnEntrarLogin = new JButton("Entrar");
+		panel.add(btnEntrarLogin, "cell 0 6 7 1,alignx center,aligny center");
+		btnEntrarLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String logindigit = fieldUserLogin.getText();
+				String senhadigit = fieldSenhaLogin.getText();
+				try {
+					ControllerAdm contAdm = new ControllerAdm();
+					contAdm.verificarloginadm(logindigit);
+					contAdm.verificarsenhaadm(senhadigit);
+				} catch (BusinessException error) {
+					JOptionPane.showMessageDialog(null, error.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				} 
+
+			}
+		});
+	}
+}
