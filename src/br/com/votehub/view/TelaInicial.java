1038428b1b075a941c3fb75d70b8b573b3fa1246
heldzr <contatohjr@gmail.com>
2023-11-30 19:10:49 -0300

    melhorias visuais na tela inicial

--- src/br/com/votehub/view/TelaInicial.java
+++ src/br/com/votehub/view/TelaInicial.java
@@ -1,5 +1,7 @@
 package br.com.votehub.view;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -15,58 +17,63 @@ import java.awt.event.ActionEvent;
 
 public class TelaInicial extends JFrame {
 
-    private static final long serialVersionUID = 1L;
-    private JPanel contentPane;
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
 
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    TelaInicial frame = new TelaInicial();
-                    frame.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaInicial frame = new TelaInicial();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
-    public TelaInicial() {
-    	setExtendedState(JFrame.MAXIMIZED_BOTH);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 800, 600);
-        contentPane = new JPanel();
-        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+	public TelaInicial() {
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 600);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.DARK_GRAY);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-        setContentPane(contentPane);
-        contentPane.setLayout(new MigLayout("fill", "[][][][grow][][][]", "[][][][][][][][][][][][]"));
-                
-                        JLabel lblNewLabelVotehub = new JLabel("VoteHub");
-                        lblNewLabelVotehub.setHorizontalAlignment(SwingConstants.CENTER);
-                        lblNewLabelVotehub.setFont(new Font("Tahoma", Font.BOLD, 34));
-                        contentPane.add(lblNewLabelVotehub, "cell 3 0,alignx center,aligny bottom");
-                
-                        JButton btnNewButtonAcessoUsuario = new JButton("USUÁRIO");
-                        btnNewButtonAcessoUsuario.addActionListener(new ActionListener() {
-                            public void actionPerformed(ActionEvent e) {
-                                LoginUsuario loginUsuario = new LoginUsuario();
-                                loginUsuario.setVisible(true);
-                                dispose();
-                            }
-                        });
-                        btnNewButtonAcessoUsuario.setFont(new Font("Tahoma", Font.PLAIN, 30));
-                        contentPane.add(btnNewButtonAcessoUsuario, "cell 3 4,alignx center");
-                
-                        JButton btnNewButtonAcessoAdm = new JButton("ADMINISTRADOR");
-                        btnNewButtonAcessoAdm.setFont(new Font("Tahoma", Font.PLAIN, 30));
-                        contentPane.add(btnNewButtonAcessoAdm, "cell 3 7,alignx center");
-        btnNewButtonAcessoAdm.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                ADMLogin admLogin = new ADMLogin();
-                admLogin.setVisible(true);
-                dispose();
-            }
-        });
-    }
+		setContentPane(contentPane);
+		contentPane.setLayout(new MigLayout("fill", "[][][][][][grow][][][][][]", "[][][][][][][][][][][][]"));
+
+		JPanel panel = new JPanel();
+		contentPane.add(panel, "cell 4 0 2 1,alignx center,growy");
+		panel.setPreferredSize(new Dimension(800, 600));
+		panel.setLayout(new MigLayout("fill", "[grow][][grow][][][][][grow]", "[][][][][][][][][][][]"));
+
+		JLabel lblNewLabelVotehub = new JLabel("VoteHub");
+		panel.add(lblNewLabelVotehub, "cell 1 0 5 1,alignx center,aligny center");
+		lblNewLabelVotehub.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabelVotehub.setFont(new Font("Tahoma", Font.BOLD, 34));
+
+		JButton btnNewButtonAcessoUsuario = new JButton("USUÁRIO");
+		panel.add(btnNewButtonAcessoUsuario, "cell 1 3 5 1,alignx center,aligny center");
+		btnNewButtonAcessoUsuario.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				LoginUsuario loginUsuario = new LoginUsuario();
+				loginUsuario.setVisible(true);
+				dispose();
+			}
+		});
+		btnNewButtonAcessoUsuario.setFont(new Font("Tahoma", Font.PLAIN, 30));
+
+		JButton btnNewButtonAcessoAdm = new JButton("ADMINISTRADOR");
+		panel.add(btnNewButtonAcessoAdm, "cell 2 5 4 1,alignx center,aligny center");
+		btnNewButtonAcessoAdm.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnNewButtonAcessoAdm.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ADMLogin admLogin = new ADMLogin();
+				admLogin.setVisible(true);
+				dispose();
+			}
+		});
+	}
 }
-
