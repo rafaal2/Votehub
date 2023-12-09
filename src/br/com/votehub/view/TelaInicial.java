6ed55883be6cb5c3bef6807476b1c0c2586c3cfa
eron <eron13.1reis@gmail.com>
2023-12-09 18:08:50 -0300

    Correção TelaInicial

--- src/br/com/votehub/view/TelaInicial.java
+++ src/br/com/votehub/view/TelaInicial.java
@@ -51,47 +51,54 @@ public class TelaInicial extends JFrame {
 		setContentPane(contentPane);
 		
 
-		ImageIcon icon = new ImageIcon("G:\\Kaio\\Downloads Chrome\\figurinha\\3179218.png");
+		ImageIcon icon = new ImageIcon("./icons/cedula.png");
+		ImageIcon adm = new ImageIcon("./icons/adm.png");
+		ImageIcon user = new ImageIcon("./icons/user.png");
 
 		// Resize the image
 		Image image = icon.getImage().getScaledInstance(150, 150, Image.SCALE_SMOOTH);
+		Image admImg = adm.getImage().getScaledInstance(70, 70, Image.SCALE_SMOOTH);
+		Image userImg = user.getImage().getScaledInstance(70, 70, Image.SCALE_SMOOTH);
 		ImageIcon resizedIcon = new ImageIcon(image);
-		getContentPane().setLayout(new MigLayout("", "[409px][383px]", "[487px]"));
+		ImageIcon resizedAdm = new ImageIcon(admImg);
+		ImageIcon resizedUser = new ImageIcon(userImg);
+		getContentPane().setLayout(new MigLayout("fill", "[409px][383px]", "[487px]"));
 
 		JPanel panelEsquerda = new JPanel();
 		panelEsquerda.setBackground(new Color(192, 192, 192));
 		getContentPane().add(panelEsquerda, "cell 0 0,grow");
-		panelEsquerda.setLayout(new MigLayout("", "[46px][][][][][][][][][][][][][][][]", "[14px][][][][][][]"));
+		panelEsquerda.setLayout(new MigLayout("fill", "[46px][][][][][][][][][][][][][][][]", "[14px][][][][][][]"));
 
 		JLabel lblIcone = new JLabel(resizedIcon);
 		lblIcone.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		panelEsquerda.add(lblIcone, "cell 8 5,alignx left,aligny top");
+		panelEsquerda.add(lblIcone, "cell 8 3,alignx left,aligny top");
 
 		JLabel lblTitulo = new JLabel(" VOTEHUB");
 		lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTitulo.setFont(new Font("Tahoma", Font.BOLD, 20));
-		panelEsquerda.add(lblTitulo, "cell 8 6");
+		panelEsquerda.add(lblTitulo, "cell 8 4");
 
 		JPanel panelDireita = new JPanel();
 		panelDireita.setBackground(new Color(164, 247, 176));
 		getContentPane().add(panelDireita, "cell 1 0,grow");
 		panelDireita.setLayout(
-				new MigLayout("", "[][][][grow][grow][][][grow][][]", "[][][][][][grow][grow][][grow][][][]"));
+				new MigLayout("fill", "[][][][grow][grow][][][grow][][]", "[][][][grow][][grow][grow][][grow][][][grow]"));
 
 		JLabel lblTituloPanel2 = new JLabel("Seja bem-vindo!");
 		lblTituloPanel2.setFont(new Font("Tahoma", Font.BOLD, 20));
-		panelDireita.add(lblTituloPanel2, "cell 4 2");
+		panelDireita.add(lblTituloPanel2, "cell 4 2 2 1,alignx center");
 
-		JLabel lblAcesseComoAdm = new JLabel("Acesse como administrador");
-		lblAcesseComoAdm.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		panelDireita.add(lblAcesseComoAdm, "cell 4 4");
+		JLabel lblAcesseComoAdm = new JLabel("Acesse como administrador:");
+		lblAcesseComoAdm.setFont(new Font("Tahoma", Font.BOLD, 14));
+		panelDireita.add(lblAcesseComoAdm, "cell 4 4,alignx center,aligny center");
 
 		JPanel panelBtnAdm = new JPanel();
 		panelBtnAdm.setBackground(new Color(192, 192, 192));
 		panelDireita.add(panelBtnAdm, "cell 4 5 2 1, grow, alignx center");
 		panelBtnAdm.setLayout(new GridLayout(1, 0, 0, 0));
-
-		JButton btnAcessoAdm = new JButton("ACESSAR");
+		
+		
+		JButton btnAcessoAdm = new JButton(resizedAdm);
 		panelBtnAdm.add(btnAcessoAdm);
 		btnAcessoAdm.addActionListener((ActionListener) new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -101,9 +108,9 @@ public class TelaInicial extends JFrame {
 			}
 		});
 
-		JLabel lblAcesseComoVotante = new JLabel("Acesse como votante");
-		lblAcesseComoVotante.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		panelDireita.add(lblAcesseComoVotante, "cell 4 7");
+		JLabel lblAcesseComoVotante = new JLabel("Acesse como votante:");
+		lblAcesseComoVotante.setFont(new Font("Tahoma", Font.BOLD, 14));
+		panelDireita.add(lblAcesseComoVotante, "cell 4 7,alignx center,aligny center");
 
 		JPanel panelBtnVotante = new JPanel();
 		panelBtnVotante.setBackground(new Color(192, 192, 192));
@@ -111,7 +118,7 @@ public class TelaInicial extends JFrame {
 		panelBtnVotante.setLayout(new GridLayout(1, 0, 0, 0));
 		
 
-		JButton btnAcessoVotante = new JButton("ACESSAR");
+		JButton btnAcessoVotante = new JButton(resizedUser);
 		panelBtnVotante.add(btnAcessoVotante);
 		btnAcessoVotante.addActionListener((ActionListener) new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
