76798a395adc771d9a84943921610be9908794b7
rafael <rafaelmarinhoalves5@gmail.com>
2023-12-14 15:13:42 -0300

    teste

--- src/testes/TesteControllerADM.java
+++ src/testes/TesteControllerADM.java
@@ -1,8 +1,7 @@
 package testes;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.sql.SQLException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
