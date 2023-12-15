1976369c9a2f22ae3f42dbe2ae82cb39a31df5a9
Kaio Vinícius <kaiovsbarbosa@gmail.com>
2023-12-15 19:29:08 -0300

    Estilização de menu de apuração de votos

--- src/br/com/votehub/view/Apuracao.java
+++ src/br/com/votehub/view/Apuracao.java
@@ -2,6 +2,7 @@ package br.com.votehub.view;
 
 import java.awt.Color;
 import java.awt.Cursor;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,6 +13,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
+import net.miginfocom.swing.MigLayout;
 
 public class Apuracao extends JFrame {
 
@@ -24,39 +26,29 @@ public class Apuracao extends JFrame {
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
 		setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
 		setFont(new Font("Tahoma", Font.PLAIN, 13));
-		setTitle("apurar votos\r\n");
+		setTitle("Apurar votos\r\n");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 501, 381);
+		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
-		contentPane.setBackground(Color.LIGHT_GRAY);
+		contentPane.setBackground(new Color(164, 247, 176));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setForeground(Color.LIGHT_GRAY);
 		contentPane.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		contentPane.setLayout(new MigLayout("fill", "[grow][][][][][][][][][][grow][][grow]", "[][][][][][][][][]"));
 		
-		JLabel lblNewLabel = new JLabel("apuração de resultados");
-		lblNewLabel.setBounds(284, 24, 228, 27);
-		lblNewLabel.setVerticalAlignment(SwingConstants.TOP);
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 22));
-		contentPane.add(lblNewLabel);
+		JPanel panel = new JPanel();
+		contentPane.add(panel, "cell 6 0,alignx center,aligny center");
+		panel.setPreferredSize(new Dimension(800, 600));
+		panel.setLayout(new MigLayout("fill", "[grow][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][grow][][grow]", "[][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][]"));
 		
-		JButton btnApurarDiretor = new JButton("Resultado Diretor");
-		btnApurarDiretor.setBounds(340, 108, 135, 33);
-		contentPane.add(btnApurarDiretor);
-		btnApurarDiretor.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				ApurarDiretor diretor = new ApurarDiretor();
-				diretor.setVisible(true);
-				dispose();
-			}
-		});
+		JLabel lblTitulo = new JLabel("Apuração de resultados");
+		panel.add(lblTitulo, "cell 26 1,alignx center");
+		lblTitulo.setVerticalAlignment(SwingConstants.TOP);
+		lblTitulo.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		
 		JButton btnApurarReitor = new JButton("Resultado Reitor");
-		btnApurarReitor.setBounds(342, 177, 133, 33);
-		contentPane.add(btnApurarReitor);
+		panel.add(btnApurarReitor, "cell 26 10,grow");
 		btnApurarReitor.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
@@ -66,20 +58,8 @@ public class Apuracao extends JFrame {
 			}
 		});
 		
-		JButton btnVoltar = new JButton("VOLTAR");
-		btnVoltar.setBounds(44, 319, 100, 23);
-		contentPane.add(btnVoltar);
-		btnVoltar.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-    			ADMPrincipal admp = new ADMPrincipal();
-    			admp.setVisible(true);
-    			dispose();
-    		}
-    	});
-		
 		JButton btnApurarProposta = new JButton("Resultado Propostas");
-		btnApurarProposta.setBounds(340, 257, 135, 33);
-		contentPane.add(btnApurarProposta);
+		panel.add(btnApurarProposta, "cell 26 15,grow");
 		btnApurarProposta.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
@@ -88,5 +68,26 @@ public class Apuracao extends JFrame {
 				dispose();
 			}
 		});
+		
+		JButton btnApurarDiretor = new JButton("Resultado Diretor");
+		panel.add(btnApurarDiretor, "cell 26 20,grow");
+		btnApurarDiretor.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				ApurarDiretor diretor = new ApurarDiretor();
+				diretor.setVisible(true);
+				dispose();
+			}
+		});
+		
+		JButton btnVoltar = new JButton("VOLTAR");
+		panel.add(btnVoltar, "cell 26 40,growx,aligny center");
+		btnVoltar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+    			ADMPrincipal admp = new ADMPrincipal();
+    			admp.setVisible(true);
+    			dispose();
+    		}
+    	});
 	}
 }
