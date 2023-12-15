6960e2167860f13e3c637536839158c2ff63ff1e
Kaio Vinícius <kaiovsbarbosa@gmail.com>
2023-12-15 07:07:50 -0300

    Correção de cor na tela ADMCadCandidato

--- src/br/com/votehub/view/ADMCadCandidato.java
+++ src/br/com/votehub/view/ADMCadCandidato.java
@@ -55,7 +55,7 @@ public class ADMCadCandidato extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 501, 381);
 		contentPane = new JPanel();
-		contentPane.setBackground(new Color(128, 128, 128));
+		contentPane.setBackground(new Color(164, 247, 176));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
