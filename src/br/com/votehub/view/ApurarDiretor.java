eeb150c37879dbb31a567523907fdedec5322ca0
Kaio Vinícius <kaiovsbarbosa@gmail.com>
2023-12-16 23:04:28 -0300

    Estilização da tela ApurarDiretor

--- src/br/com/votehub/view/ApurarDiretor.java
+++ src/br/com/votehub/view/ApurarDiretor.java
@@ -1,7 +1,6 @@
 package br.com.votehub.view;
-
 import java.awt.Color;
-import java.awt.EventQueue;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,171 +11,173 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Vector;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.SwingConstants;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 
 import br.com.votehub.controller.ControllerCandidato;
 import br.com.votehub.model.DAOs.DB;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
+import net.miginfocom.swing.MigLayout;
 
 public class ApurarDiretor extends JFrame {
-	Connection conn = null;
-	Statement st = null;
-	ResultSet rs = null;
-	PreparedStatement stt = null;
-	PreparedStatement stt1 = null;
 
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-	private JTable table;
-	private JComboBox<String> comboBoxVotacao;
+    private static final long serialVersionUID = 1L;
+
+    Connection conn = null;
+    Statement st = null;
+    ResultSet rs = null;
+    PreparedStatement stt = null;
+    PreparedStatement stt1 = null;
+
+    private JPanel contentPane;
+    private JTable table;
+    private JComboBox<String> comboBoxVotacao;
     private DefaultTableModel tableModel = new DefaultTableModel();
     private JLabel lblNrVotos = new JLabel();
     private String idVotacaoSelecionada;
 
-	public ApurarDiretor() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setExtendedState(JFrame.MAXIMIZED_BOTH);
-		setBounds(100, 100, 846, 413);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+    public static void main(String[] args) {
+        ApurarDiretor frame = new ApurarDiretor();
+        frame.setVisible(true);
+    }
 
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JLabel lblVotacao = new JLabel("Selecione a votação :");
-        lblVotacao.setBounds(137, 100, 170, 30);
+    public ApurarDiretor() {
+        setTitle("Apuração do Diretor");
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        contentPane = new JPanel();
+        contentPane.setBackground(new Color(164, 247, 176));
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+        setContentPane(contentPane);
+        contentPane.setLayout(new MigLayout("fill", "[grow]", "[][][][][][][][][][][][]"));
+
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.WHITE);
+        contentPane.add(panel, "alignx center,aligny center");
+        panel.setPreferredSize(new Dimension(800, 600));
+        panel.setLayout(new MigLayout("fill", "[grow]", "[][][][][][][][][][][][]"));
+
+        JLabel lblTitulo = new JLabel("Resultado da votação de diretor");
+        lblTitulo.setHorizontalAlignment(JLabel.CENTER);
+        lblTitulo.setFont(new Font("Tahoma", Font.PLAIN, 21));
+        panel.add(lblTitulo, "cell 0 0, growx, wrap");
+
+        JLabel lblVotacao = new JLabel("Selecione a votação:");
         lblVotacao.setFont(new Font("Tahoma", Font.BOLD, 12));
-        contentPane.add(lblVotacao);
+        panel.add(lblVotacao, "cell 0 2, alignx center, wrap");
 
-        comboBoxVotacao = new JComboBox();
-        comboBoxVotacao.setBounds(323, 100, 170, 30);
-        contentPane.add(comboBoxVotacao);
+        comboBoxVotacao = new JComboBox<>();
+        panel.add(comboBoxVotacao, "cell 0 2, alignx center, wrap");
         comboBoxVotacao.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-            	idVotacaoSelecionada = (String) comboBoxVotacao.getSelectedItem();
+                idVotacaoSelecionada = (String) comboBoxVotacao.getSelectedItem();
                 apurarVotosDiretor();
                 atualizarNumeroVotos(idVotacaoSelecionada);
             }
         });
         restaurarvotacaoCombobox();
 
-		JLabel lblNewLabel = new JLabel("Resultado Votação de Diretor");
-		lblNewLabel.setBounds(5, 5, 820, 26);
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 21));
-		contentPane.add(lblNewLabel);
+        lblNrVotos = new JLabel("Número total de votos:");
+        lblNrVotos.setFont(new Font("Tahoma", Font.BOLD, 12));
+        panel.add(lblNrVotos, "cell 0 4, alignx center, wrap");
 
-		lblNrVotos = new JLabel("numero total de votos :");
-		lblNrVotos.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblNrVotos.setBounds(137, 150, 350, 14);
-		contentPane.add(lblNrVotos);
+        tableModel = new DefaultTableModel();
+        table = new JTable(tableModel) {
+            private static final long serialVersionUID = 1L;
 
-		tableModel = new DefaultTableModel();
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
 
-		table = new JTable(tableModel) {
-		    public boolean isCellEditable(int row, int column) {
-		        return false;
-		    }
-		};
-		tableModel.setColumnIdentifiers(new Object[] { "Número do Candidato", "Nome", "Votos"});
-		table.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		table.setDoubleBuffered(true);
-		table.getColumnModel().getColumn(0).setPreferredWidth(114);
-		table.setBackground(Color.LIGHT_GRAY);
-		table.setBounds(137, 106, 548, 80);
-		contentPane.add(table);
+        tableModel.setColumnIdentifiers(new Object[] { "Número do Candidato", "Nome", "Votos" });
+        table.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+        table.setDoubleBuffered(true);
+        table.getColumnModel().getColumn(0).setPreferredWidth(114);
+        table.setBackground(Color.LIGHT_GRAY);
+        panel.add(table, "cell 0 6, grow, span, wrap");
 
-		apurarVotosDiretor();
-		//atualizarNumeroVotos();
-		JScrollPane scrollPane = new JScrollPane(table);
-		scrollPane.setBounds(137, 200, 548, 80);
-		contentPane.add(scrollPane);
+        apurarVotosDiretor();
+        JScrollPane scrollPane = new JScrollPane(table);
+        panel.add(scrollPane, "cell 0 8, grow, span, wrap");
 
-		JButton btnVoltar = new JButton("VOLTAR");
-		btnVoltar.setBounds(10, 300, 89, 23);
-		contentPane.add(btnVoltar);
-		btnVoltar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Apuracao apvoto = new Apuracao();
-				apvoto.setVisible(true);
-				dispose();
-			}
-		});
-	}
+        JButton btnVoltar = new JButton("VOLTAR");
+        panel.add(btnVoltar, "cell 0 10, alignx center, wrap");
+        btnVoltar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Apuracao apvoto = new Apuracao();
+                apvoto.setVisible(true);
+                dispose();
+            }
+        });
+    }
 
-	 public void apurarVotosDiretor() {
-	        if (tableModel == null) {
-	            tableModel = new DefaultTableModel(); // Inicialize se for nulo
-	        }
-	        tableModel.setRowCount(0);
-	        //String idVotacaoSelecionada = (String) comboBoxVotacao.getSelectedItem();
-	        try {
-	            conn = DB.getConnection();
-	            st = conn.createStatement();
-	            rs = st.executeQuery("SELECT\r\n"
-	                    + "	  candidato.numero_candidato,\r\n"
-	                    + "	  candidato.nome AS nome_candidato,\r\n"
-	                    + "	  COUNT(voto.id_voto) AS numero_de_votos\r\n"
-	                    + "	  FROM\r\n"
-	                    + "		    candidato\r\n"
-	                    + "	  LEFT JOIN\r\n"
-	                    + "		    voto ON candidato.numero_candidato = voto.numero_candidato\r\n"
-	                    + "	  WHERE\r\n"
-	                    + "		    candidato.cargo = 'Diretor'\r\n"
-	                    + "   AND candidato.id_votacao = " + idVotacaoSelecionada + "\r\n"
-	                    + "	  GROUP BY\r\n"
-	                    + "		    candidato.numero_candidato, candidato.nome\r\n"
-	                    + "	  ORDER BY\r\n"
-	                    + "		    numero_de_votos DESC;");
+    public void apurarVotosDiretor() {
+        if (tableModel == null) {
+            tableModel = new DefaultTableModel();
+        }
+        tableModel.setRowCount(0);
+        try {
+            conn = DB.getConnection();
+            st = conn.createStatement();
+            rs = st.executeQuery("SELECT\r\n" + "	  candidato.numero_candidato,\r\n"
+                    + "	  candidato.nome AS nome_candidato,\r\n"
+                    + "	  COUNT(voto.id_voto) AS numero_de_votos\r\n" + "	  FROM\r\n"
+                    + "		    candidato\r\n" + "	  LEFT JOIN\r\n"
+                    + "		    voto ON candidato.numero_candidato = voto.numero_candidato\r\n" + "	  WHERE\r\n"
+                    + "		    candidato.cargo = 'Reitor'\r\n" + "   AND candidato.id_votacao = "
+                    + idVotacaoSelecionada + "\r\n" + "	  GROUP BY\r\n"
+                    + "		    candidato.numero_candidato, candidato.nome\r\n" + "	  ORDER BY\r\n"
+                    + "		    numero_de_votos DESC;");
 
-	            while (rs.next()) {
-	                Vector<Object> row = new Vector<>();
-	                row.add(rs.getString("numero_candidato"));
-	                row.add(rs.getString("nome_candidato"));
-	                row.add(rs.getInt("numero_de_votos"));
-	                tableModel.addRow(row);
-	            }
-	        } catch (SQLException e) {
-	            e.printStackTrace();
-	        } finally {
-	            DB.closeResultSet(rs);
-	            DB.closestatement(st);
-	        }
-	    }
+            while (rs.next()) {
+                Vector<Object> row = new Vector<>();
+                row.add(rs.getString("numero_candidato"));
+                row.add(rs.getString("nome_candidato"));
+                row.add(rs.getInt("numero_de_votos"));
+                tableModel.addRow(row);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DB.closeResultSet(rs);
+            DB.closestatement(st);
+        }
+    }
 
-	 public void atualizarNumeroVotos(String idVotacao) {
-	        try {
-	            conn = DB.getConnection();
-	            st = conn.createStatement();
-	            rs = st.executeQuery("SELECT\r\n" + "    COUNT(voto.id_voto) AS numero_total_votos\r\n" + "FROM\r\n"
-	                    + "    voto\r\n" + "LEFT JOIN\r\n"
-	                    + "    candidato ON voto.numero_candidato = candidato.numero_candidato\r\n" + "WHERE\r\n"
-	                    + "    candidato.cargo = 'Diretor' AND candidato.id_votacao = " + idVotacao + ";");
+    public void atualizarNumeroVotos(String idVotacao) {
+        try {
+            conn = DB.getConnection();
+            st = conn.createStatement();
+            rs = st.executeQuery("SELECT\r\n" + "    COUNT(voto.id_voto) AS numero_total_votos\r\n" + "FROM\r\n"
+                    + "    voto\r\n" + "LEFT JOIN\r\n"
+                    + "    candidato ON voto.numero_candidato = candidato.numero_candidato\r\n" + "WHERE\r\n"
+                    + "    candidato.cargo = 'Reitor' AND candidato.id_votacao = " + idVotacao + ";");
 
-	            if (rs.next()) {
-	                int numeroTotalVotos = rs.getInt("numero_total_votos");
-	                lblNrVotos.setText("Número total de votos na Votação de Diretor: " + numeroTotalVotos);
-	            }
-	        } catch (SQLException e) {
-	            e.printStackTrace();
-	        } finally {
-	            DB.closeResultSet(rs);
-	            DB.closestatement(st);
-	        }
-	    }
-	
-	public void restaurarvotacaoCombobox() {
+            if (rs.next()) {
+                int numeroTotalVotos = rs.getInt("numero_total_votos");
+                lblNrVotos.setText("Número total de votos na Votação de Reitor: " + numeroTotalVotos);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DB.closeResultSet(rs);
+            DB.closestatement(st);
+        }
+    }
+
+    public void restaurarvotacaoCombobox() {
         try {
             ControllerCandidato objCand = new ControllerCandidato();
             comboBoxVotacao.removeAllItems();
